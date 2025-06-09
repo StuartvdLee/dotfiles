275d089aa86a0877c6bfd86e2a4f9d0023e1a7ad
Stuart van der Lee <stuartvanderlee92+github@gmail.com>
2025-06-09 12:07:32 +0200

    Add pre-commit hook

--- VSCode/pre_commit_hook.py
+++ VSCode/pre_commit_hook.py
@@ -5,7 +5,7 @@ import sys
 def save_code_extensions():
     """Save the list of installed VSCode extensions to ./VSCode/code_extensions"""
     print("Saving VSCode extensions…")
-    with open("code_extensions", "w") as f:
+    with open("VSCode/code_extensions", "w") as f:
         subprocess.run(
             ["code", "--list-extensions"],
             stdout=f,
@@ -15,6 +15,7 @@ def save_code_extensions():
 def main():
     home = os.path.expanduser("~")
     dotfiles_dir = os.path.join(home, "Repositories/dotfiles")
+    save_dir = os.path.join(dotfiles_dir, "VSCode")
 
     # Compare absolute paths
     current_working_dir = os.path.abspath(os.getcwd())
@@ -22,9 +23,9 @@ def main():
 
     if current_working_dir == expected_dir:
         save_code_extensions()
-        print("Adding code_extensions to git…")
+        print("Adding VS Code extensions…")
         subprocess.run(
-            ["git", "add", "code_extensions"],
+            ["git", "add", "VSCode/code_extensions"],
             check=True
         )
     else:
